f5898762f6c26f0cee3b4053ef3c83482fb76ca0
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-03-01 22:18:56 +0530

    Add files via upload

---
+++ pages/Country.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState, useTransition } from 'react'
+import { getCountryData } from '../API/postApi';
+import Loader from "../components/UI/Loader.jsx";
+import CountryCard from '../components/Layout/CountryCard.jsx';
+
+const Country = () => {
+
+  const [isPending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData();
+      // getCountryData();
+      setCountries(res.data)
+    })
+  }, [])
+
+  if (isPending) { return <h1><Loader /></h1> }
+
+  return (
+    <section className='country-section'>
+      <ul className='grid grid-four-cols'>
+        {
+          countries.map((curCountry, index) => {
+            return (
+              <CountryCard country={curCountry} key={index} />
+            )
+          })
+        }
+      </ul>
+    </section>
+  )
+}
+
+export default Country
